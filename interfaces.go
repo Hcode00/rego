bb06320e61fef62e5ee12df8116d883fa11a0ee0
Hcode00 <es-hazemahmed2025@alexu.edu.eg>
2023-11-03 23:04:49 +0200

    Usable Module

---
+++ interfaces.go
@@ -0,0 +1,56 @@
+package rego
+
+type Page struct {
+	Lang string
+	Head Head
+	Body Body
+	HTML string
+}
+type Head struct {
+	Title  string
+	Link   []Link
+	Script []Script
+	Meta   []Meta
+	Style  []Style
+	HTML   string
+}
+type Body struct {
+	Attr     string
+	Elements []Element
+	HTML     string
+}
+type Link struct {
+	Rel  string
+	Type string
+	Href string
+	As   string
+	HTML string
+}
+type Meta struct {
+	Name     string
+	Content  string
+	Charset  string
+	Property string
+	HTML     string
+}
+type Style []string
+type Script struct {
+	Type string
+	Attr string
+	Src  string
+	HTML string
+}
+type Element struct {
+	Tag     string
+	Attr    string
+	Class   string
+	Id      string
+	Content interface{}
+	HTML    string
+}
+
+type HTMLServer struct {
+	templates map[string]Page
+}
+
+
---
+++ server.go
@@ -0,0 +1,48 @@
+package rego
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func NewHTMLServer() *HTMLServer {
+	return &HTMLServer{
+		templates: make(map[string]Page),
+	}
+}
+
+func (s *HTMLServer) RegisterTemplate(path string, template Page) {
+	s.templates[path] = template
+}
+
+func (s *HTMLServer) ServeHTML(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	template, exists := s.templates[path]
+	if !exists {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write([]byte(NOT_FOUND_TEMPLATE))
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write([]byte(template.GetTemplate()))
+}
+
+func (s *HTMLServer) ServeStaticFile(w http.ResponseWriter, r *http.Request) {
+    filename := r.URL.Path[len("/static/"):]
+    http.ServeFile(w, r, STATIC_DIR+"/"+filename)
+}
+
+func (s *HTMLServer) RegisterHTML(path string, html string) {
+    s.templates[path] = Page{HTML: html}
+}
+
+
+func (s *HTMLServer) StartRouter(port int) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/static/", s.ServeStaticFile)
+	mux.HandleFunc("/", s.ServeHTML)
+	addr := fmt.Sprintf(":%d", port)
+	fmt.Printf("Starting server on port %d...\n", port)
+	http.ListenAndServe(addr, mux)
+}
